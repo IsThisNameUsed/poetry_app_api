7a85035a94309f962853b4bab0693c4ecca87a8a
Pierre <haro@netc.fr>
2022-01-06 18:30:39 +0100

    first commit

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,20 @@
+﻿namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int IsComplete { get; set; }
+
+        public String? secret { get; set; }
+    }
+
+
+    public class TodoItemDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int IsComplete { get; set; }
+    }
+
+}
